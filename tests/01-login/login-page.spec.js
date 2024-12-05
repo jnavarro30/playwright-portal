6fe0db5e9804395a8e922362ac0499d31fcab4fe
jnavarro30 <jnavarro30@live.com>
2024-12-04 16:34:30 -0800

    login page

--- tests/01-login/login-page.spec.js
+++ tests/01-login/login-page.spec.js
@@ -4,21 +4,15 @@ import Login from "../00-screen-objects/01-login.js";
 test("log in", async ({ page }) => {
   await Login.logIn(page);
   await expect(page).toHaveURL(/.*dashboard/);
-  // await page.pause();
 });
 
 // unable to find, icons blocking Resend Text link
-// test('resend code', async ({ page }) => {
-//   await page.goto('https://portal-stage.independa.com/');
-//   await page.getByPlaceholder('Phone number').fill('4040404040');
-//   await page.getByRole('button', { name: 'Login via Text' }).click();
-//   await page.getByText('Resend Text').click();
-//   await expect(page.getByText("Success !")).toBeVisible();
-
-//   // await page.getByPlaceholder('Enter Email').fill('jesse.cg@independa.com');
-//   // await page.getByPlaceholder('Enter Password').fill('ind@123');
-//   // await page.getByRole('button', { name: 'Login'}).click();
-//   // await expect(page).toHaveURL(/.*dashboard/);
+// test("resend code", async ({ page }) => {
+//   await page.goto("https://portal-stage.independa.com/");
+//   await page.getByPlaceholder("Phone number").fill("4040404040");
+//   await page.getByRole("button", { name: "Login via Text" }).click();
+//   await page.getByText("Resend Text").click();
+//   await expect(page.getByText("Success!")).toBeVisible();
 // });
 
 test("log out", async ({ page }) => {
@@ -28,37 +22,38 @@ test("log out", async ({ page }) => {
   await expect(page).toHaveURL("https://portal-stage.independa.com/");
 });
 
-  test('open facebook in a new tab', async ({ page }) => {
-    await page.goto('https://portal-stage.independa.com/');
-    await page.getByRole('link').first().click();
-    const newTab = await page.waitForEvent("popup");
-    await expect(newTab).toHaveURL('https://www.facebook.com/Independa');
-  });
+test("try another number", async ({ page }) => {
+  await page.goto("https://portal-stage.independa.com/");
+  await page.getByPlaceholder("Phone number").fill("4040404040");
+  await page.getByRole("button", { name: "Login via Text" }).click();
+  await page.getByText("Try with another number").click();
+  await expect(page.getByRole("button", { name: "Login via Text" })).toBeVisible();
+});
 
-  test('open x in a new tab', async ({ page }) => {
-    await page.goto('https://portal-stage.independa.com/');
-    await page.getByRole('link').nth(1).click();
-    const newTab = await page.waitForEvent("popup");
-    await expect(newTab).toHaveURL('https://x.com/independa');
-  });
+test("open facebook in a new tab", async ({ page }) => {
+  await page.goto("https://portal-stage.independa.com/");
+  await page.getByRole("link").first().click();
+  const newTab = await page.waitForEvent("popup");
+  await expect(newTab).toHaveURL("https://www.facebook.com/Independa");
+});
 
-  test('open linkedin in a new tab', async ({ page }) => {
-    await page.goto('https://portal-stage.independa.com/');
-    await page.getByRole('link').nth(2).click();
-    const newTab = await page.waitForEvent("popup");
-    await expect(newTab).toHaveURL('https://www.linkedin.com/company/independa-inc-');
-  });
+test("open x in a new tab", async ({ page }) => {
+  await page.goto("https://portal-stage.independa.com/");
+  await page.getByRole("link").nth(1).click();
+  const newTab = await page.waitForEvent("popup");
+  await expect(newTab).toHaveURL("https://x.com/independa");
+});
 
-  test('open youtube in a new tab', async ({ page }) => {
-    await page.goto('https://portal-stage.independa.com/');
-    await page.locator('a').filter({ hasText: 'Youtube-colorCreated with' }).click();
-    const newTab = await page.waitForEvent("popup");
-    await expect(newTab).toHaveURL('https://www.youtube.com/user/Independa4U');
-  });
+test("open linkedin in a new tab", async ({ page }) => {
+  await page.goto("https://portal-stage.independa.com/");
+  await page.getByRole("link").nth(2).click();
+  const newTab = await page.waitForEvent("popup");
+  await expect(newTab).toHaveURL("https://www.linkedin.com/company/independa-inc-");
+});
 
-//   test('forgot password', async ({ page }) => {
-//     await page.goto('https://portal.independa.com/');
-//     await page.getByText('Forgot your password').click();
-//     const newTab = await page.waitForEvent("popup");
-//     await expect(newTab).toHaveURL('https://www.youtube.com/user/Independa4U');
-//   });
+test("open youtube in a new tab", async ({ page }) => {
+  await page.goto("https://portal-stage.independa.com/");
+  await page.locator("a").filter({ hasText: "Youtube-colorCreated with" }).click();
+  const newTab = await page.waitForEvent("popup");
+  await expect(newTab).toHaveURL("https://www.youtube.com/user/Independa4U");
+});
